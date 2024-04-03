9c9621937c185429d067401875481d4f2ed9990e
FarishaAkma <nurfarishaakma@gmail.com>
2024-04-03 14:14:31 +0800

    Coding 2

--- coding2.c
+++ coding2.c
@@ -8,22 +8,7 @@ int main (void){
 void sigint_handler(int sig);
 void sigtstp_handler(int sig);
 void sigquit_handler(int sig);
-void sigeof_handler(int sig);
 char s[200];
-
-FILE *fp;
-int c;
-
-if ((fp=fopen("coding2.txt","rb"))==NULL){
-	perror("coding2.txt");
-	return 0;
-}
-
-while ((c=fgetc(fp))!=EOF)
-{
-	putchar(c);
-	sigeof_handler(EOF);
-}
 
 if (signal(SIGINT,sigint_handler)==SIG_ERR){
 	perror("signal");
@@ -47,7 +32,6 @@ if (fgets(s,200,stdin)==NULL)
 else
 	printf("you entered: %s\n", s);
 
-fclose(fp);
 return 0;
 }
 
@@ -61,8 +45,4 @@ void sigtstp_handler(int sig){
 
 void sigquit_handler(int sig){
         printf("This is a special signal handler for SIGQUIT\n");
-}
-
-void sigeof_handler(int sig){
-        printf("This is a special signal handler for EOF\n");
 }
